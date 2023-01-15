79505d725c6fdc8041d6da8df74b71e382dbfb6d
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2023-01-15 15:04:09 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+systemctl start docker
+    
